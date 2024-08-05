80ba1374ea658c5819d2d2eb7f2899c45ccaeb58
h-willis <harry.willis@btconnect.com>
2024-08-05 13:34:46 +1200

    display due date on TodoItems

--- frontend/src/TodoItem.js
+++ frontend/src/TodoItem.js
@@ -6,15 +6,11 @@ function TodoItem({ todo, onClick, handleDelete }) {
     handleDelete(todo._id);
   }
 
-  // function handleUpdate() {
-
-  // }
-
   return (<>
     <div className={`todoItem ${todo.completed ? 'complete' : ''}`} onClick={() => onClick(todo._id)}>
       <div className='todoHeader'>
         <h2>{todo.title}</h2>
-        {/* <button id="update" onClick={handleUpdate}>...</button> */}
+        {todo.dueDate === null ? null : <p>{todo.dueDate.toDateString()}</p>}
         <button onClick={(e) => _handleDelete(e)}>X</button>
       </div>
       <p>{todo.description}</p>
